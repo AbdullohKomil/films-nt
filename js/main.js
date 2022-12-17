f8ae2f6e2a0727de8d8aa3b9b0f1963d24a59926
AbdullohKomil <akomiljonov@icloud.com>
2022-12-17 20:27:42 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,172 @@
+// var users = [
+//   {
+//     name: "Abdulloh",
+//     debt: 10000,
+//   },
+//   {
+//     name: "Bekzod",
+//     debt: 20000,
+//   },
+//   {
+//     name: "Rustam",
+//     debt: 35000,
+//   },
+//   {
+//     name: "Jamshid",
+//     debt: 50000,
+//   },
+//   {
+//     name: "Bunyod",
+//     debt: 5000,
+//   },
+// ];
+
+// const newArr = users.map((el) => {
+//   return {
+//     name: el.name + 'bek',
+//     debt: el.debt + '$'
+//   };
+// })
+// console.log(newArr);
+
+// const newArr = users.filter((el) => el.name.length == 6);
+// console.log(newArr);
+
+// var arr = [true,false,true,false,true,]
+
+// const newArr = arr.filter((el) => el == true)
+
+// console.log(newArr.length);
+
+// var numbers = [1, 2, 3, 4, 5, 6];
+
+// const arr = [1, "asass", true, undefined, 2, null, "", "fdksj"];
+// const arr2 = ["1", "asass", true, undefined, false, null, "", "fdksj"];
+
+// --------------------------------------------UY ISHI------------------------------------------------------------------
+
+
+function render(array, node) {
+  // node.innerHTML = "";
+  array.forEach((item) => {
+    var newBox = document.createElement("div");
+    newBox.classList.add(
+      "col-3",
+      "text-center",
+      "text-white",
+      "bg-success",
+      "mb-5",
+      "ms-2",
+      "text-light",
+    );
+    var appendBox = elRow.appendChild(newBox);
+  
+    var elImg = document.createElement("img");
+    elImg.setAttribute("src", item.poster);
+    elImg.setAttribute("width", "200px");
+    newBox.appendChild(elImg);
+  
+    var elTitle = document.createElement("h4");
+    elTitle.innerHTML = item.title;
+    newBox.appendChild(elTitle);
+  
+    var elRelease = document.createElement("p");
+    elRelease.innerHTML = item.release_date;
+    elRelease.classList.add("text-warning")
+    newBox.appendChild(elRelease)
+  
+    var elGenr = document.createElement("p")
+    elGenr.innerHTML = item.genres
+    elGenr.classList.add('w-100')
+    newBox.appendChild(elGenr)
+  });
+}
+var elRow = document.querySelector(".row");
+
+render(films, elRow);
+var elSelect = document.querySelector("#select-js");
+
+let newArr = [];
+
+films.forEach((item) => {
+  var newBox = document.createElement("div");
+  var elRow = document.querySelector(".row");
+  newBox.classList.add(
+    "col-3",
+    "text-center",
+    "text-white",
+    "bg-success",
+    "mb-5",
+    "ms-2",
+    "text-light",
+  );
+  var appendBox = elRow.appendChild(newBox);
+
+  var elImg = document.createElement("img");
+  elImg.setAttribute("src", item.poster);
+  elImg.setAttribute("width", "200px");
+  newBox.appendChild(elImg);
+
+  var elTitle = document.createElement("h4");
+  elTitle.innerHTML = item.title;
+  newBox.appendChild(elTitle);
+
+  var elRelease = document.createElement("p");
+  elRelease.innerHTML = item.release_date;
+  elRelease.classList.add("text-warning")
+  newBox.appendChild(elRelease)
+
+  var elGenr = document.createElement("p")
+  elGenr.innerHTML = item.genres
+  elGenr.classList.add('w-100')
+  newBox.appendChild(elGenr)
+});
+
+var elSelect = document.querySelector("#select-js")
+elSelect.addEventListener("change", function () {
+  newArr = [];
+
+  if (elSelect.value != "All") {
+    films.forEach((film) => {
+      if (film.genres.includes(elSelect.value)) {
+        newArr.push(film);
+        render(newArr, elRow);
+      }
+    });
+    render(newArr, elRow);
+  }else {
+    render(films, elRow);
+  }
+});
+
+var selectOptions = [];
+let newSet = new Set();
+films.forEach((item) => {
+  item.genres.forEach((type) => {
+    newSet.add(type);
+    // console.log(type);
+  });
+});
+// console.log(newSet);
+newSet.forEach((type) => {
+  var elOption = document.createElement("option");
+  elOption.value = type;
+  elOption.textContent = type;
+  elSelect.appendChild(elOption);
+});
+
+var elForm = document.querySelector(".js-form")
+var elInput = document.querySelector(".js-search")
+let Arr2  =[]
+elForm.addEventListener('input', (evt) => {
+  elRow.innerHTML = ""
+  evt.preventDefault()
+  let elInputVal = elInput.value.toLocaleLowerCase(); 
+  films.forEach((el) => {
+    if(el.title.toLocaleLowerCase().includes(elInputVal)) {
+      newArr.push(el)
+    }
+  })
+  render(newArr ,)
+  newArr = []
+})
